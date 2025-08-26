4de36de1418ac271751da8df033914023f03f327
Daniel Baigel <dbaigel@yext.com>
2025-08-26 16:17:31 -0400

    fixed lint errors

--- components/pages-overview-dashboard.tsx
+++ components/pages-overview-dashboard.tsx
@@ -1440,7 +1440,8 @@ export function PagesOverviewDashboard() {
                   <div className="relative group">
                     <Info className="h-4 w-4 text-muted-foreground cursor-help hover:text-foreground" />
                     <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-gray-900 text-white text-sm rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-50">
-                      Generic search terms that don't include your brand name
+                      Generic search terms that don&apos;t include your brand
+                      name
                       <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900"></div>
                     </div>
                   </div>
